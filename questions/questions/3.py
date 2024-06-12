38cf856870604752fc419ac14cdf37042784f5e6
Aditya pippal <122475891+adityap5@users.noreply.github.com>
2024-06-12 22:34:23 +0530

    day3

---
+++ questions/questions/3.py
@@ -0,0 +1,27 @@
+#1 print string in reverse, its length , in uppercase,lowercase and copy it into another string
+
+# String = input("enter a string : ")
+
+# copy = String
+# rev=""
+# for i in range(len(String)-1,-1,-1):
+#     rev = rev + String[i]
+# print(rev)
+# print(String.upper())
+# print(String.lower())
+# print(len(String))
+# print(copy)
+# print(String[::-1]) # reverse method
+
+#2. vowels in str
+
+# a = "hello how are you doin"
+
+# vowel =0
+# const=0
+# for i in a:
+#     if i in "aeiouAEIOU":
+#         vowel = vowel + 1
+#     else: const = const +1    
+# print(vowel , const)  
+
